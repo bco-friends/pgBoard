85d1915514518688884b6d245e3e6f989c5e610b
Jeremy Ward <jeremy@jmichaelward.com>
2023-08-25 17:37:01 -0500

    Configure PHPUnit and set up a test class and wrapper command.

---
+++ tests/BoardParseTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace PgBoard\PgBoard\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BoardParseTest extends TestCase
+{
+  public function testItWorks()
+  {
+    self::assertTrue(true);
+  }
+}
