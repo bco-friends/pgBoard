a8c4fc4eed7dce198e5e672381b52b7e4eb7bff7
Jeremy Ward <jeremy@jmichaelward.com>
2023-09-01 12:50:19 -0500

    Add a data provider and tests for the Base::type method.

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -8,6 +8,22 @@ use PHPUnit\Framework\TestCase;
 
 class BaseTest extends TestCase
 {
+  public static function getTypes(): array
+  {
+    return [
+      [ Base::LIST_THREAD, 'thread' ],
+      [ Base::VIEW_THREAD, 'thread' ],
+      [ Base::LIST_THREAD_HISTORY, 'thread' ],
+      [ Base::LIST_THREAD_SEARCH, 'thread'],
+      [ Base::VIEW_MESSAGE, 'message'],
+      [ Base::LIST_MESSAGE_HISTORY, 'message'],
+      [ Base::LIST_MESSAGE_SEARCH, 'message'],
+      [ Base::LIST_MESSAGE_HISTORY, 'message'],
+      [ Base::VIEW_MESSAGE_SEARCH, 'message'],
+      [ PHP_INT_MAX, null]
+    ];
+  }
+
   /**
    * @covers Base::init
    * @test
@@ -15,5 +31,18 @@ class BaseTest extends TestCase
   public function it_creates_a_class_instance()
   {
     self::assertInstanceOf(Base::class, Base::init());
+  }
+
+  /**
+   * @dataProvider getTypes
+   * @covers Base::type
+   * @test
+   */
+  public function it_sets_the_table_property(int $type, ?string $expected_table)
+  {
+    $base = Base::init();
+    $base->type($type);
+
+    self::assertSame($expected_table, $base->table);
   }
 }
