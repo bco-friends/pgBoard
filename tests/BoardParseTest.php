ad68aea5aa9c4209df0c512ea060a5e352f9a5dc
Jeremy Ward <jeremy@jmichaelward.com>
2023-09-03 12:16:07 -0500

    Add test cases for non-hidden media except Twitter, for now.

--- tests/BoardParseTest.php
+++ tests/BoardParseTest.php
@@ -72,6 +72,27 @@ class BoardParseTest extends TestCase
     return new BoardParse($this->bbc, $this->rep, false);
   }
 
+  public static function getShowMediaEmbeds(): array {
+    return [
+      'img' => [
+        '[img]https://i.pinimg.com/originals/24/01/ed/2401ed63791dcbafdd7f5fd9a3d07c75.jpg[/img]',
+        '<img src="https://i.pinimg.com/originals/24/01/ed/2401ed63791dcbafdd7f5fd9a3d07c75.jpg" ondblclick="window.open(this.src);"/>',
+      ],
+      'youtube' => [
+        '[youtube]https://youtu.be/VbhZZnIRPOI?si=x7H3P2hdz4LmJRef[/youtube]',
+        '<object width="425" height="355"><param name="movie" value="https://youtube.com/v/VbhZZnIRPOI?si=x7H3P2hdz4LmJRef"></param><param name="wmode" value="transparent"></param><embed src="https://youtube.com/v/VbhZZnIRPOI?si=x7H3P2hdz4LmJRef" type="application/x-shockwave-flash" wmode="transparent" width="425" height="355"></embed></object>'
+      ],
+      'vimeo' => [
+        '[vimeo]https://vimeo.com/15262898[/vimeo]',
+        '<iframe src="https://player.vimeo.com/video/15262898?title=0&amp;byline=0&amp;portrait=0" width="425" height="239" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
+      ],
+      'soundcloud' => [
+        '[soundcloud]https://soundcloud.com/subpop/pissed-jeans-false-jesii-part?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing[/soundcloud]',
+        '<object height="81" width="100%"><param name="wmode" value="opaque"><param name="movie" value="http://player.soundcloud.com/player.swf?url=https%3A%2F%2Fsoundcloud.com%2Fsubpop%2Fpissed-jeans-false-jesii-part%3Futm_source%3Dclipboard%26amp%3Butm_medium%3Dtext%26amp%3Butm_campaign%3Dsocial_sharing"><param name="allowscriptaccess" value="always"><embed allowscriptaccess="always" height="81" src="http://player.soundcloud.com/player.swf?url=https%3A%2F%2Fsoundcloud.com%2Fsubpop%2Fpissed-jeans-false-jesii-part%3Futm_source%3Dclipboard%26amp%3Butm_medium%3Dtext%26amp%3Butm_campaign%3Dsocial_sharing" type="application/x-shockwave-flash" width="100%"></object>',
+      ],
+    ];
+  }
+
   public static function getHideMediaEmbeds(): array {
     return [
       'img' => [
@@ -143,5 +164,15 @@ class BoardParseTest extends TestCase
   public function it_creates_links_when_media_is_hidden(string $link_text, string $expected_html)
   {
     self::assertSame($expected_html, (new BoardParse($this->bbc, $this->rep, true))->run($link_text));
+  }
+
+  /**
+   * @covers BoardParse::run
+   * @dataProvider getShowMediaEmbeds
+   * @test
+   */
+  public function it_converts_embeds_when_media_is_not_hidden(string $link_text, string $expected_html)
+  {
+    self::assertSame($expected_html, $this->getDefaultParser()->run($link_text));
   }
 }
