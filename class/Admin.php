bde3ef581f35974cca0c949b0064823c51ef7187
Jeremy Ward <jeremy@jmichaelward.com>
2023-09-03 10:26:16 -0500

    Update methods to use class database instance instead of global.

--- class/Admin.php
+++ class/Admin.php
@@ -7,8 +7,7 @@ class BoardAdmin
 
   function toggle_flag($table,$flag,$id)
   {
-    global $DB;
-    $DB->query("UPDATE
+    $this->DB->query("UPDATE
                   {$table}
                 SET
                   {$flag}=(CASE WHEN {$flag} IS true THEN false ELSE true END)
@@ -18,7 +17,6 @@ class BoardAdmin
 
   function check_flag($table,$flag,$id)
   {
-    global $DB;
-    return $DB->value("SELECT {$flag} FROM {$table} WHERE id=$1",array($id)) == "t" ? true : false;
+    return $this->DB->value("SELECT {$flag} FROM {$table} WHERE id=$1",array($id)) == "t" ? true : false;
   }
 }
