2b260e3e16d34d62a2654105d97acd947f8b56fa
Jeremy Ward <jeremy@jmichaelward.com>
2023-09-03 19:09:38 -0500

    Scaffold Symfony Console command for seeding data.

---
+++ class/src/CommandService.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace PgBoard\PgBoard;
+
+use PgBoard\PgBoard\Command\DatabaseSeeder;
+use Symfony\Component\Console\Application;
+
+class CommandService
+{
+  private array $commands = [
+    DatabaseSeeder::class,
+  ];
+
+  public function __construct(
+    private readonly Application $application
+  ) {}
+
+  public static function load() {
+    $application = new Application();
+    $service = new self($application);
+    $service->load_commands();
+  }
+
+  public function load_commands()
+  {
+    foreach ($this->commands as $command_class) {
+      $this->application->add(new $command_class());
+    }
+
+    $this->application->run();
+  }
+}
