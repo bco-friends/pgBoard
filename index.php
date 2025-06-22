cf5cd18c6afb0aced5c88546a641fdde56c54f05
Jeremy Ward <jeremy@jmichaelward.com>
2025-06-22 13:29:37 -0500

    Move modified files into config directory and ignore.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $__start__ = microtime(true);
-require_once("config.php");
+require_once __DIR__ . '/config/config.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
